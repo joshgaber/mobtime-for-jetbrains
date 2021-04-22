66b5f60974fedccbcc64b36a56d4e7028883540b
GitHub Action <action@github.com>
2021-04-22 03:38:23 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/joshgaber/mobtimeforjetbrains/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.joshgaber.mobtimeforjetbrains.services
+
+import com.github.joshgaber.mobtimeforjetbrains.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
